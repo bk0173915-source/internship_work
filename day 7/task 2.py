69534e3e08b9f391521de70c200d8d4f6e5c2f84
bk0173915-source <bk0173915@gmail.com>
2026-02-10 22:14:25 +0530

    internship work

---
+++ day 7/task 2.py
@@ -0,0 +1,21 @@
+# Permission Checker
+
+def admin_only(func):
+    def wrapper(username):
+        if username == "admin":
+            return func(username)
+        else:
+            print("Access Denied")
+    return wrapper
+
+
+@admin_only
+def dashboard(username):
+    print("Welcome to Admin Dashboard,", username)
+
+
+print("Admin Access:")
+dashboard("admin")
+
+print("\nNormal User Access:")
+dashboard("madan")
---
+++ day 7/task 3.py
@@ -0,0 +1,46 @@
+# Student College System
+
+class Student:
+
+    college_name = "ABC College"   # Class Variable (shared)
+
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+    @classmethod
+    def change_college(cls, new_name):
+        cls.college_name = new_name
+
+    @staticmethod
+    def is_pass(marks):
+        if marks >= 35:
+            return "Pass"
+        else:
+            return "Fail"
+
+    def display(self):
+        print("Name:", self.name)
+        print("Roll No:", self.roll_no)
+        print("College:", Student.college_name)
+        print()
+        
+
+s1 = Student("Madan", 101)
+s2 = Student("Rahul", 102)
+
+print("Before Changing College:\n")
+s1.display()
+s2.display()
+
+Student.change_college("XYZ College")
+
+print("After Changing College:\n")
+s1.display()
+s2.display()
+
+print("Result Check:")
+print("Madan:", Student.is_pass(75))
+print("Rahul:", Student.is_pass(30))
+
+
